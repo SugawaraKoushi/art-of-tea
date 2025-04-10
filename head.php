dbc5e1e84e872369f545da8e86feb55248ac892a
Vladislav <senior.vlados97@gmail.com>
2025-04-10 19:24:31 +0300

    first commit

---
+++ head.php
@@ -0,0 +1,16 @@
+<?php header("Content-type: text/html;charset=utf-8"); ?>
+<!DOCTYPE html>
+<html lang="ru">
+<title><?php echo $title ?></title>
+
+<head>
+    <link rel="stylesheet" href="./styles.css" />
+</head>
+
+<body>
+    <header class="header-layout"></header>
+    <main></main>
+    <footer></footer>
+</body>
+
+</html>
