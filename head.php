2bc18dddda2afe30b80ba02258fead278c90d4a6
Vladislav <senior.vlados97@gmail.com>
2025-05-10 20:17:10 +0300

    Move to php

--- head.php
+++ head.php
@@ -4,13 +4,13 @@
 <title><?php echo $title ?></title>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="./styles.css" />
+    <script src="./scripts/BannerSlider.js" defer></script>
+    <script src="./scripts/ShowButton.js"></script>
+    <script src="./scripts/ShowModal.js"></script>
+    <script src="./scripts/ProductCounter.js" defer></script>
+    <script src="./scripts/Mobile.js" defer></script>
 </head>
 
 <body>
-    <header class="header-layout"></header>
-    <main></main>
-    <footer></footer>
-</body>
-
-</html>
---
+++ main.php
@@ -0,0 +1,41 @@
+<main>
+    <div class="layout">
+        <div class="slider-container">
+            <div class="slider" id="slider1">
+                <div class="slide" onclick="showModal()">
+                    <img class="slide-background" src="images/slide 1 bg.png" alt="ng" />
+                    <h1 class="title">ПЕРСОНАЛЬНЫЙ АСИСТЕНТ</h1>
+                    <button class="button-base-secondary very-large" id="open-modal">
+                        <img src="icons/fill/20/stars.svg" alt="" />
+                        Ввести запрос
+                    </button>
+                    <img class="avatar" src="images/avatar.png" alt="avatar" />
+                    <div class="message">
+                        Добрый день!</br>
+                        Я Ваш личный ассистент</br>
+                        Чем я могу помочь?
+                    </div>
+                </div>
+                <div class="slide">
+                    <img class="slide-background" src="images/slide 2 bg.png" alt="">
+                </div>
+            </div>
+            <div class="pagination-slider" id="pagination-slider">
+                <div class="pagination-point active" data-index="0" data-slider="slider1"></div>
+                <div class="pagination-point" data-index="1" data-slider="slider1"></div>
+                <div class="pagination-point" data-index="2" data-slider="slider1"></div>
+                <div class="pagination-point" data-index="3" data-slider="slider1"></div>
+            </div>
+        </div>
+    </div>
+    <button class="button-floating hidden" id="open-modal-floating" onclick="showModal()">
+        <img src="/images/avatar 2.png" alt="">
+    </button>
+
+    <?php if (empty($messages)) {
+        require "modal_new.php";
+    } else {
+        require "modal_chat.php";
+    } ?>
+
+</main>
